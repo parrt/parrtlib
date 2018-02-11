1deae0bc97af31d2f6b9dccb8e641f1e4e32f29b
parrt <parrt@cs.usfca.edu>
2018-02-11 10:56:29 -0800

    change exec sig

--- src/us/parr/lib/ParrtSys.java
+++ src/us/parr/lib/ParrtSys.java
@@ -6,29 +6,30 @@
 
 package us.parr.lib;
 
-import us.parr.lib.util.Pair;
 import us.parr.lib.util.StreamVacuum;
 
 import java.io.File;
 
 public class ParrtSys {
 	/** Given an executable command name and arguments, execute the process
-	 *  and return the {exit code (as a string), stdout, stderr} in
-	 *  a String array.
+	 *  and return the {stdout, stderr} in a String array.
 	 *
 	 *  The returned output strings are "" if empty, not null.
 	 */
-	public static Pair<String, String> exec(String execPath, String... args)
-		throws Exception
-	{
-		Process process = Runtime.getRuntime().exec(args, null, new File(execPath));
-		StreamVacuum stdoutVacuum = new StreamVacuum(process.getInputStream());
-		StreamVacuum stderrVacuum = new StreamVacuum(process.getErrorStream());
-		stdoutVacuum.start();
-		stderrVacuum.start();
-		process.waitFor();
-		stdoutVacuum.join();
-		stderrVacuum.join();
-		return new Pair<>(stdoutVacuum.toString(), stderrVacuum.toString());
+	public static String[] exec(String execPath, String... args) {
+		try {
+			Process process = Runtime.getRuntime().exec(args, null, new File(execPath));
+			StreamVacuum stdoutVacuum = new StreamVacuum(process.getInputStream());
+			StreamVacuum stderrVacuum = new StreamVacuum(process.getErrorStream());
+			stdoutVacuum.start();
+			stderrVacuum.start();
+			process.waitFor();
+			stdoutVacuum.join();
+			stderrVacuum.join();
+			return new String[]{stdoutVacuum.toString(), stderrVacuum.toString()};
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
